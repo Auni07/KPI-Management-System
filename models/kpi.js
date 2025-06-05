3c5e1b29c34b2f3f8954517372f42ec067aa1497
Auni07 <auninosman90@gmail.com>
2025-06-05 20:20:17 +0800

    refactor(kpi): add more kpi for staff

--- models/kpi.js
+++ models/kpi.js
@@ -1,66 +1,31 @@
 const mongoose = require('mongoose');
 
-const progressUpdateSchema = new mongoose.Schema({
-  progressInput: { type: String },
-  progressNote: { type: String },
-  file: {
-    filePath: { type: String },
-    fileNote: { type: String }
-  },
-  createdAt: { //current date
-    type: Date,
-    default: Date.now
-  }
-});
-
 const kpiSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String
-  },
-  target: { //indicator + target value
-    type: String
-  },
-  targetValue: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  progress: {
-    type: String
-  },
-  progressNumber: {
-    type: Number,
-    min: 0,
-    default: 0
-  },
-  startDate: {
-    type: Date,
-    required: true
-  },
-  dueDate: {
-    type: Date,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ['Not Started', 'In Progress', 'Completed'],
-    required: true
-  },
-  approvalstat: {
-    type: String,
-    enum: ['Pending Approval', 'Approved', 'Rejected', 'No New Progress'],
-    required: true
-  },
-  assignedTo: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true
-  },
-  progressUpdates: [progressUpdateSchema]
+  title: { type: String, required: true },
+  description: String,
+  target: String,
+  targetValue: { type: Number, min: 0, default: 0 },
+  progress: String,
+  progressNumber: { type: Number, min: 0, default: 0 },
+  startDate: { type: Date, required: true },
+  dueDate: { type: Date, required: true },
+  status: { type: String, enum: ['Not Started', 'In Progress', 'Completed'], required: true },
+  approvalstat: { type: String, enum: ['Pending Approval', 'Approved', 'Rejected', 'No New Progress'], required: true },
+  assignedTo: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  progressUpdates: [
+    {
+      progressInput: String,
+      progressNote: String,
+      file: {
+        filePath: String,
+        fileNote: String
+      },
+      createdAt: {
+        type: Date,
+        default: Date.now
+      }
+    }
+  ]
 });
 
 module.exports = mongoose.model('Kpi', kpiSchema);
